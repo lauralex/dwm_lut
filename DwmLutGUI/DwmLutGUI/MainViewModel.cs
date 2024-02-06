0db1cb36ed2f92a054564350ce70029cd1774d93
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2024-02-06 09:08:21 +0800

    Fix crash when a configured monitor is not connected (#51)
    If some monitor exists in `config.xml`, but is not connected to the computer,
    the control flow will reach MainViewModel.cs:232, where the program tries to
    set `newMonitorData.HdrLuts = new ObservableCollection<string>(hdrLutPaths)`.
    
    However, since `<hdr_luts />` may not be present in `config.xml`, `hdrLutPaths`
    may be `null`. This will result in a `System.ArgumentNullException` being
    thrown during construction of the `ObservableCollection<string>`.
    
    This patch fixes the crash by only setting `newMonitorData.HdrLuts` when the
    corresponding `hdrLutPaths` is not `null`.

--- DwmLutGUI/DwmLutGUI/MainViewModel.cs
+++ DwmLutGUI/DwmLutGUI/MainViewModel.cs
@@ -232,11 +232,9 @@ namespace DwmLutGUI
 
                     var sdrLutPaths = monitor.Element("sdr_luts")?.Elements("sdr_lut").Select(x => (string)x).ToList();
                     var hdrLutPaths = monitor.Element("hdr_luts")?.Elements("hdr_lut").Select(x => (string)x).ToList();
-                    var newMonitorData = new MonitorData(path, sdrLutPath, hdrLutPath)
-                    {
-                        SdrLuts = new ObservableCollection<string>(sdrLutPaths),
-                        HdrLuts = new ObservableCollection<string>(hdrLutPaths)
-                    };
+                    var newMonitorData = new MonitorData(path, sdrLutPath, hdrLutPath);
+                    if (sdrLutPaths != null) newMonitorData.SdrLuts = new ObservableCollection<string>(sdrLutPaths);
+                    if (hdrLutPaths != null) newMonitorData.HdrLuts = new ObservableCollection<string>(hdrLutPaths);
                     _allMonitors.Add(newMonitorData);
                 }
             }
